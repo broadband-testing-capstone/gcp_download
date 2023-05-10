1777daaa363c490dcf4f8d836a6111e31832e30d
dodsonta <71948722+dodsonta@users.noreply.github.com>
2023-05-10 16:25:48 -0700

    json2csv done

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 ```gsutil -m cp "gs://[bucket name]/*" [download location] ```
 replace [bucket name] with the name of the bucket you want to copy from and [download location] with the location you want to download the files to inside google shell.
 3. Click more (the three dots) in google shell and then select download, it will have you select the location of the folder or file you want to download and then have you select the download location.
-4. Run json2csv.py for m_lab json files and txt2csv.py for netrics txt files. Each require -i and -o flags for input and output file locations respectively.
+4. Run json2csv.py for m_lab json files and txt2csv.py for netrics txt files. You can use -i and -o options to specify input and output files. If you don't specify either it will be in the current directory.
--- json2csv.py
+++ json2csv.py
@@ -2,7 +2,9 @@ import sys
 import getopt
 import os
 import json
+import csv
 
+#get user arguments
 def get_args(argv):
 	input = ''
 	output = ''
@@ -17,23 +19,66 @@ def get_args(argv):
 			output = argv
 	return input, output
 
+#reads a json file and returns a json dictionary
 def json2csv(file):
 	fo = open(file, 'r')
 	data = json.load(fo)
 	fo.close()
 	return data
 
+#writes to a csv file
+def writeCSV(folder, file, jsonArray):
+	joined = os.path.join(folder, file)
+	fo = open(os.path.join(joined), 'w', newline='')
+	writer = csv.writer(fo)
+	writer.writerow(jsonArray[0].keys())
+	for row in jsonArray:
+		writer.writerow(row.values())
+	fo.close()
+
+#homogenizes the data between the different tests
+def cleanData(data):
+	allowedKeys = ["TestName", "TestStartTime", "TestEndTime",
+			   "MurakamiLocation", "MurakamiConnectionType", "MurakamiNetworkType",
+			    "DownloadValue", "DownloadUnit",	"UploadValue", "UploadUnit"]
+	ogKeys = list(data.keys())
+	for key in ogKeys:
+		if key not in allowedKeys:
+			data.pop(key)
+	return data
+
 if __name__ == '__main__':
+	print("Running")
 	argv = sys.argv[1:]
-	input, output = get_args(argv)
+	#input, output = get_args(argv)
+	input = 'C:/Users/Taite/Desktop/CapstoneFiles/gcp_files/mlab/'
+	output = 'C:/Users/Taite/Desktop/CapstoneFiles/gcp_files/'
 	if input == '':
 		input = './'
 	if output == '':
 		output = './'
 	files = os.listdir(input)
-	jsonArray = []
+	ndt5Array = []
+	ndt7Array = []
+	multiArray = []
+	singleArray = []
 	for file in	files:
 		if file.endswith('.jsonl'):
 			joined = os.path.join(input, file)
 			jsonData = json2csv(joined)
-			jsonArray.append(a)
+			if "TestError" in jsonData: #Checking if there was any errors, skipping the file if there was
+				continue
+			jsonData = cleanData(jsonData)
+			if 'ndt5' in file:
+				ndt5Array.append(jsonData)
+			elif 'ndt7' in file:
+				ndt7Array.append(jsonData)
+			elif 'multi-stream' in file:
+				multiArray.append(jsonData)
+			elif 'single-stream' in file:
+				singleArray.append(jsonData)
+	writeCSV(output, 'ndt5.csv', ndt5Array)
+	writeCSV(output, 'ndt7.csv', ndt7Array)
+	writeCSV(output, 'multi-stream.csv', multiArray)
+	writeCSV(output, 'single-stream.csv', singleArray)
+	print("Done")
